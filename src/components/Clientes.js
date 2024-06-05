bd61316df7247efc6a27c7eae58488ffba604d03
YunjoOmega <140874849+YunjoOmega@users.noreply.github.com>
2024-06-05 11:57:42 -0500

    Segundo Commit: LocalStorage de Clientes

--- src/components/Clientes.js
+++ src/components/Clientes.js
@@ -1,75 +1,267 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './Clientes.css'
 
 const Clientes = () => {
-  return (
+    const [ formData, setFormData ] = useState({
+        primerNombre: '',
+        segundoNombre: '',
+        primerApellido: '',
+        segundoApellido: '',
+        tipoDoc: '',
+        numeroDocu: '',
+        fechaNac: '',
+        cel: '',
+        tel: '',
+        email: '',
+        pais: '',
+        departamento: '',
+        municipio: '',
+        direccion: '',
+    })
+
+    const [usuarios, setUsuarios] = useState([])
+
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        setFormData((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }))
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const newUsuarios = [...usuarios, formData]
+        setUsuarios(newUsuarios)
+        localStorage.setItem('usuarios', JSON.stringify(newUsuarios))
+        setFormData({
+            primerNombre: '',
+            segundoNombre: '',
+            primerApellido: '',
+            segundoApellido: '',
+            tipoDoc: '',
+            numeroDocu: '',
+            fechaNac: '',
+            cel: '',
+            tel: '',
+            email: '',
+            pais: '',
+            departamento: '',
+            municipio: '',
+            direccion: '',
+        })
+    }
+
+    useEffect(() => {
+        const storedUsuarios = localStorage.getItem('usuarios');
+        if (storedUsuarios) {
+            setUsuarios(JSON.parse(storedUsuarios));
+        }
+    }, [])
+
+    return (
     <div className='formulario'>
         <h2>Formulario de Creación de Usuario</h2>
-    <form action="#" method="post">
+    <form onSubmit={handleSubmit}>
         <div class="input-group">
-            <label for="nombre" id="primer_nombre">Primer Nombre:</label>
-            <input type="text"  name="nombre" required/>
+            <label htmlFor='primerNombre'>Primer Nombre:</label>
+            <input 
+            type="text"
+            id='primerNombre'
+            name="primerNombre"
+            value={formData.primerNombre}
+            onChange={handleChange} 
+            required
+            />
         </div>
         <div class="input-group">
-            <label for="nombre">Segundo Nombre:</label>
-            <input type="text" id="segundo_nombre" name="nombre"/>
+            <label htmlFor="segundoNombre">Segundo Nombre:</label>
+            <input 
+            type="text" 
+            id="segundoNombre" 
+            name="segundoNombre"
+            value={formData.segundoNombre}
+            onChange={handleChange}
+            />
         </div>
         <div class="input-group">
-            <label for="apellido">Primer Apellido:</label>
-            <input type="text" id="primer_apellido" name="apellido" required/>
+            <label htmlFor="primerApellido">Primer Apellido:</label>
+            <input 
+            type="text" 
+            id='primerApellido'
+            name='primerApellido'
+            value={formData.primerApellido}
+            onChange={handleChange} 
+            required
+            />
         </div>
         <div class="input-group">
-            <label for="apellido">Segundo Apellido:</label>
-            <input type="text" id="segundo_apellido" name="apellido"/>
+            <label htmlFor="segundoApellido">Segundo Apellido:</label>
+            <input 
+            type="text" 
+            id="segundoApellido" 
+            name="segundoApellido"
+            value={formData.segundoApellido}
+            onChange={handleChange}
+            />
         </div>
         <div class="input-group">
-            <label for="documento">Tipo De Documento</label>
-            <select name="tipo_doc" required>
+            <label htmlFor="tipoDoc">Tipo De Documento</label>
+            <select 
+            name="tipoDoc"
+            value={formData.tipoDoc}
+            onChange={handleChange} 
+            required
+            >
                 <option value="">Seleccione un tipo de documento</option>
                 <option value="DNI">Cedula de Ciudadania</option>
                 <option value="LC">Cedula de Extranjeria</option>
             </select>
         </div>
         <div class="input-group">
-            <label for="numeroDocu">Número de Documento:</label>
-            <input type="number" id="numeroDocu" name="numeroDocu" required/>
+            <label htmlFor="numeroDocu">Número de Documento:</label>
+            <input 
+            type="number" 
+            id="numeroDocu" 
+            name="numeroDocu" 
+            value={formData.numeroDocu}
+            onChange={handleChange}
+            required
+            />
         </div>
         <div class="input-group">
-            <label for="fechaNac">Fecha de Nacimiento:</label>
-            <input type="date" id="fechaNac" name="fechaNac" required/>
+            <label htmlFor="fechaNac">Fecha de Nacimiento:</label>
+            <input 
+            type="date" 
+            id="fechaNac" 
+            name="fechaNac" 
+            value={formData.fechaNac}
+            onChange={handleChange}
+            required
+            />
         </div>
         <div class="input-group">
-            <label for="cel">Número de Celular:</label>
-            <input type="number" id="cel" name="cel" required/>
+            <label htmlFor="cel">Número de Celular:</label>
+            <input 
+            type="number" 
+            id="cel" 
+            name="cel" 
+            value={formData.cel}
+            onChange={handleChange}
+            required
+            />
         </div>
         <div class="input-group">
-            <label for="tel">Número de Teléfono:</label>
-            <input type="number" id="tel" name="tel" required/>
+            <label htmlFor="tel">Número de Teléfono:</label>
+            <input 
+            type="number"
+            id="tel" 
+            name="tel" 
+            value={formData.tel}
+            onChange={handleChange}
+            required
+            />
         </div>
         <div class="input-group">
-            <label for="email">Correo Electrónico:</label>
-            <input type="email" id="email" name="email" required/>
+            <label htmlFor="email">Correo Electrónico:</label>
+            <input 
+            type="email" 
+            id="email" 
+            name="email" 
+            value={formData.email}
+            onChange={handleChange}
+            required
+            />
         </div>
         <div class="input-group">
-            <label for="pais">Pais:</label>
-            <input type="text" id="pais" name="pais" required/>
+            <label HtmlFor="pais">Pais:</label>
+            <input 
+            type="text" 
+            id="pais" 
+            name="pais" 
+            value={formData.pais}
+            onChange={handleChange}
+            required
+            />
         </div>
         <div class="input-group">
-            <label for="departamento">Departamento:</label>
-            <input type="text" id="departamento" name="departamento" required/>
+            <label htmlFor="departamento">Departamento:</label>
+            <input 
+            type="text" 
+            id="departamento" 
+            name="departamento" 
+            value={formData.departamento}
+            onChange={handleChange}
+            required
+            />
         </div>
         <div class="input-group">
-            <label for="municipio">Municipio:</label>
-            <input type="text" id="municipio" name="municipio" required/>
+            <label htmlFor="municipio">Municipio:</label>
+            <input 
+            type="text" 
+            id="municipio" 
+            name="municipio"
+            value={formData.municipio}
+            onChange={handleChange} 
+            required
+            />
         </div>
         <div class="input-group">
-            <label for="direccion">Dirección:</label>
-            <input type="text" id="direccion" name="direccion" required/>
+            <label htmlFor="direccion">Dirección:</label>
+            <input 
+            type="text" 
+            id="direccion" 
+            name="direccion" 
+            value={formData.direccion}
+            onChange={handleChange}
+            required
+            />
         </div>
-        <div class="input-group">
+        <div className="input-group">
             <button type="submit">Crear Usuario</button>
         </div>
     </form>
+    <h2>Usuarios Creados</h2>
+    <table>
+        <thead>
+            <tr>
+                <th>Primer Nombre</th>
+                <th>Segundo Nombre</th>
+                <th>Primer Apellido</th>
+                <th>segundo Apellido</th>
+                <th>Tipo de Documento</th>
+                <th>Numero de Documento</th>
+                <th>Fecha de Nacimiento</th>
+                <th>Numero de Celular</th>
+                <th>Numero de Teléfono</th>
+                <th>Correo Electrónico</th>
+                <th>Pais</th>
+                <th>Departamento</th>
+                <th>Municipio</th>
+                <th>Dirección</th>
+            </tr>
+        </thead>
+        <tbody>
+            {usuarios.map((usuario, index) => (
+                <tr key={index}>
+                    <td>{usuario.primerNombre}</td>
+                    <td>{usuario.segundoNombre}</td>
+                    <td>{usuario.primerApellido}</td>
+                    <td>{usuario.segundoApellido}</td>
+                    <td>{usuario.tipoDoc}</td>
+                    <td>{usuario.numeroDocu}</td>
+                    <td>{usuario.fechaNac}</td>
+                    <td>{usuario.cel}</td>
+                    <td>{usuario.tel}</td>
+                    <td>{usuario.email}</td>
+                    <td>{usuario.pais}</td>
+                    <td>{usuario.departamento}</td>
+                    <td>{usuario.municipio}</td>
+                    <td>{usuario.direccion}</td>
+                </tr>
+            ))}
+        </tbody>
+    </table>
     </div>
   )
 }
